4641dccd3e2052d2920763f84418e4ff1dd187b1
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-02-16 13:34:11 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-	/*
-	 * main is the main program part,entery point
-	 * print "\"Programming is like building a multilingual puzzle"
-	 * not allowed to use printf
-	 * program should end with the value 0
-	 */
+/*
+* main is the main program part,entery point
+* print "\"Programming is like building a multilingual puzzle"
+* not allowed to use printf
+* program should end with the value 0
+*/
 
 
 int main(void)
