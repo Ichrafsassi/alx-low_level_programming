e121488d827bca253a17f02fad1021e606afd9ba
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-02-16 13:36:38 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-/*
-* main is the main program part,entery point
-* print "\"Programming is like building a multilingual puzzle"
-* not allowed to use printf
-* program should end with the value 0
-*/
-
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
