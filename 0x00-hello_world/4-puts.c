3b95ef7432d72dd25b5bfc4cb12ba7f2e1bcd43c
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-02-16 13:26:35 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+	/*
+	 * this script ill print "    "Programming is like building a multilingual puzzle   "
+	 * printf is not allowed to use
+	 * program should end with the value 0
+	 */
+
+
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
         puts("\n");
         return (0);
 }
