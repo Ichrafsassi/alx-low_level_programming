a5592133846dc99454c0677f47bf70d989547484
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-02-16 13:38:33 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
+ * main - Entry point - main program
+ * prints "\"Programming is like building a multilingual puzzle"
+ * program return 0
  */
 
 int main(void)
