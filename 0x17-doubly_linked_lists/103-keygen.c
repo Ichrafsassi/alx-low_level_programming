73f94c5c8efcb4399c5806a7c44703322381805c
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-06-15 17:53:59 -0700

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,88 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-typedef struct Node
-{
-    char username[100];
-    char key[100];
-    struct Node *next;
-    struct Node *prev;
-} Node;
-
-void addNode(Node **head, char username[], char key[])
-{
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    strcpy(newNode->username, username);
-    strcpy(newNode->key, key);
-    newNode->next = NULL;
-
-    if (*head == NULL)
-    {
-        newNode->prev = NULL;
-        *head = newNode;
-    }
-    else
-    {
-        Node *temp = *head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        newNode->prev = temp;
-    }
-}
-
-char *generateKey(Node *head, char username[])
-{
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        if (strcmp(temp->username, username) == 0)
-        {
-            return temp->key;
-        }
-        temp = temp->next;
-    }
-    return NULL;
-}
-
+/**
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
-	char *key;
-	
-	if (argc == 2)
+	unsigned int i, b;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char ch[7] = "      ";
+
+	if (argc != 2)
 	{
-		Node *head = NULL;
-		addNode(&head, "julien", "javascript");
-		addNode(&head, "alice", "abcd1234");
-		addNode(&head, "bob", "password");
-		
-		key = generateKey(head, argv[1]);
-		if (key != NULL)
-		{
-			printf("%s\n", key);
-		}
-		else
-		{
-			printf("Invalid username.\n");
-		}
+		printf("Correct usage: ./keygen5 username\n");
+		return (1);
 	}
-	else if (argc == 3)
-	{
-		if (strcmp(argv[2], "javascript") == 0 && strcmp(argv[1], "julien") == 0)
-		{
-			printf("Congrats!\n");
-		}
-		else
-		{
-			printf("Invalid key.\n");
-		}
-	}
-	else
-	{
-		printf("Usage: %s username [key]\n", argv[0]);
-	}
-	return 0;
+
+	len = strlen(argv[1]);
+	ch[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+
+	ch[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= argv[1][i];
+
+	ch[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+		if ((char)b <= argv[1][i])
+			b = argv[1][i];
+	srand(b ^ 14);
+
+	ch[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+
+	ch[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+
+	ch[5] = l[(b ^ 229) & 63];
+	printf("%s\n", ch);
+	return (0);
 }
